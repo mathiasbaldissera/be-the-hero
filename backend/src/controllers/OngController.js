305fe11d35fa24232617f89d2c50b07e191400a9
mathiasbald <mathiasbaldissera@gmail.com>
2020-03-27 21:49:44 -0300

    bethehero#finishing adding validations to the backend

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,17 +1,17 @@
-const crypto = require('crypto');
-const connection = require('../database/connection');
+const generateUniqueId = require("../utils/generateUniqueId");
+const connection = require("../database/connection");
 
 async function index(request, response) {
-  const ongs = await connection('ongs').select('*');
+  const ongs = await connection("ongs").select("*");
   return response.json(ongs);
 }
 
 async function create(request, response) {
   const { name, email, whatsapp, city, uf } = request.body;
 
-  const id = crypto.randomBytes(4).toString('HEX');
+  const id = generateUniqueId();
 
-  await connection('ongs').insert({
+  await connection("ongs").insert({
     id,
     name,
     email,
